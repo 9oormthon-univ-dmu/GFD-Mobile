1d613206347c703c1b8356f9d2db6854be746986
조희우 <84004687+huiwoo-jo@users.noreply.github.com>
2024-09-29 15:11:51 +0900

    build: IconActivity

---
+++ huiwoo-jo; compose/app/src/main/java/com/brake/composetemplate/WeekSecond/IconActivity.kt
@@ -0,0 +1,53 @@
+package com.brake.composetemplate.WeekSecond
+
+import android.os.Bundle
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.brake.composetemplate.R
+import com.brake.composetemplate.ui.theme.ComposeTemplateTheme
+
+class IconActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTemplateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    IconContainer()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun IconContainer() {
+    Icon(
+        modifier = Modifier.size(24.dp),
+        imageVector = ImageVector.vectorResource(id = R.drawable.ic_android_black_24dp),
+        contentDescription = "지우기",
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IconPreview() {
+    ComposeTemplateTheme {
+        IconContainer()
+    }
+}
